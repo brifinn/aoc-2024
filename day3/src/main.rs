70ad791eb6d1cd5e01bf1a4750eca49efa12b080
brifinn <briangfinn@gmail.com>
2024-12-09 14:35:37 -0700

    add day3 skeleton and template for future days

---
+++ day3/src/main.rs
@@ -0,0 +1,10 @@
+use day2::{get_input_from_file};
+use std::process;
+
+fn main() {
+    let reports = get_reports_from_file().unwrap_or_else(|err| {
+        println!("Failed to get reports from file: {err:#?}");
+        process::exit(1)
+    });
+
+}
