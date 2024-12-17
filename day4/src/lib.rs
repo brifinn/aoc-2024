84fc8110a0c05cbb301aca7ced0fb65a5fc94629
brifinn <briangfinn@gmail.com>
2024-12-17 13:25:04 -0700

    part 2 impl'd but wrong

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -28,10 +28,10 @@ pub fn chexmas(matrix: &Vec<Vec<char>>, start: (isize, isize), direction: (isize
     let (mut row, mut col) = start;
     let (d_row, d_col) = direction;
     for c in ['X', 'M', 'A', 'S'] {
-        if row < 0 || row > matrix.len() as isize {
+        if row < 0 || row >= matrix.len() as isize {
             return false;
         }
-        if col < 0 || col > matrix[row as usize].len()  as isize {
+        if col < 0 || col >= matrix[row as usize].len()  as isize {
             return false;
         }
         // Lazy; assuming some bounds on size
@@ -43,3 +43,33 @@ pub fn chexmas(matrix: &Vec<Vec<char>>, start: (isize, isize), direction: (isize
     }
     true
 }
+
+pub fn make_cross_vectors(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> (Vec<char>, Vec<char>) {
+    let mut backslash: Vec<char> = Vec::new();
+    let mut forwardslash: Vec<char> = Vec::new();
+    for i in 0..3 {
+        backslash.push(matrix[row + i][col + i]);
+        forwardslash.push(matrix[row + i][col + 2 - i]);
+    }
+    (backslash, forwardslash)
+}
+
+pub fn check_x_mas(backslash: &Vec<char>, forwardslash: &Vec<char>) -> bool {
+    //eprintln!("backslash: {backslash:#?}, forward slash: {forwardslash:#?}");
+    let mas = vec!['M', 'A', 'S'];
+    let mut backslash_fore = backslash.iter();
+    let mut backslash_aft = backslash.iter().rev();
+    let mut forwardslash_fore = forwardslash.iter();
+    let mut forwardslash_aft = forwardslash.iter().rev();
+    let mut back_fore = true;
+    let mut back_aft = true;
+    let mut fore_fore = true;
+    let mut fore_aft = true;
+    for c in mas {
+        back_fore &= *backslash_fore.next().unwrap() == c;
+        back_aft &= *backslash_aft.next().unwrap() == c;
+        fore_fore &= *forwardslash_fore.next().unwrap() == c;
+        fore_aft &= *forwardslash_aft.next().unwrap() == c;
+    }
+    (back_fore || back_aft) && (fore_fore || fore_aft)
+ }
--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,4 +1,4 @@
-use day4::{get_input_from_file, matricize, chexmas};
+use day4::{check_x_mas, chexmas, get_input_from_file, make_cross_vectors, matricize};
 use std::process;
 
 fn main() {
@@ -19,15 +19,26 @@ fn main() {
         (1, 1),
     ];
 
-    let mut instances: usize = 0;
+    let mut xmas_instances: usize = 0;
     for row in 0..matrix.len() {
         for col in 0..matrix[row].len() {
             for direction in directions {
                 if chexmas(&matrix, (row as isize, col as isize), direction) {
-                    instances += 1;
+                    xmas_instances += 1;
                 }
             }
         }
     }
-    println!("{instances} matches");
+    println!("{xmas_instances} xmas matches");
+
+    let mut x_mas_instances: usize = 0;
+    for row in 2..matrix.len()-2 {
+        for col in 2..matrix[row].len()-2 {
+            let (backslash, forwardslash) = make_cross_vectors(&matrix, row, col);
+            if check_x_mas(&backslash, &forwardslash) {
+                x_mas_instances += 1
+            }
+        }
+    }
+    println!("{x_mas_instances} x-mas matches");
 }
